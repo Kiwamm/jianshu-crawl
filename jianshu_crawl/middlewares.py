5c0c93fbea1412eaf54f160d58c22f0628dd5f03
Kiwamm <50472785+Kiwamm@users.noreply.github.com>
2021-06-16 18:30:43 +0800

    Update middlewares.py

--- jianshu_crawl/middlewares.py
+++ jianshu_crawl/middlewares.py
@@ -3,38 +3,32 @@
 # Define here the models for your spider middleware
 #
 # See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import time
 
 from selenium import webdriver
 from scrapy.http.response.html import HtmlResponse
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.by import By
-
 
 class SeleniumDownloadMiddleware(object):
-    def __init__(self):
-        self.driver = webdriver.Chrome()
 
+    def __init__(self):
+        # 加载测试浏览器
+        self.driver = webdriver.Chrome(executable_path=r"D:\xsy的秘密小屋\Google\Google\Chrome\Application\chromedriver.exe")
+
+    # request: 则scrapy框架会去服务器加载资源
+    # reponse: 则跳过资源下载直接交给解析器方法
     def process_request(self, request, spider):
-        while True:
-            # 超时重新请求
-            try:
-                self.driver.set_page_load_timeout(1)
-                self.driver.get(request.url)
-            except:
-                pass
-            finally:
-                try:
-                    # 等待ajax加载，超时了就重来
-                    WebDriverWait(self.driver, 1).until(
-                        expected_conditions((By.CLASS_NAME, 'rEsl9f'))
-                    )
-                except:
-                    continue
-                finally:
+        self.driver.get(request.url)
+        time.sleep(2)
+        try:
+            while True:
+                showMore = self.driver.find_element_by_class_name('show-more')  # 获取标签
+                showMore.click()
+                time.sleep(0.5)
+                if not showMore:
                     break
-        url = self.driver.current_url
+        except:
+            pass
         source = self.driver.page_source
-        response = HtmlResponse(url=url, body=source, request=request, encoding='utf-8')
+        response = HtmlResponse(url=self.driver.current_url, body=source, request=request, encoding='utf-8')
         return response
